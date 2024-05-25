57460632a2890ae9b398f145cbca83b56a65a193
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-05-25 14:21:52 +0530

    file added

---
+++ src/com/example/addressbook/AddressBook.java
@@ -0,0 +1,9 @@
+package com.example.addressbook;
+
+import java.sql.SQLOutput;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Addressbook program");
+    }
+}
