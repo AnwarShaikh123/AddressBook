d33c16af2df0f8ef0857c864c6b03bf86ca89e9f
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-05-30 12:15:02 +0530

    uc2 added new contact to Addressbook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,16 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
     String firstName;
-    String LastNAme;
+    String LastName;
     String Address;
     String city;
     String state;
-    int zip;
-    int phoneNumber;
+    String zip;
+    String phoneNumber;
     String email;
 
-    public AddressBook(String firstName, String LastNAme, String Address, String city, String state, int zip, int phoneNumber, String email){
+    public AddressBook(String firstName, String LastName, String Address, String city, String state, String zip, String phoneNumber, String email){
         this.firstName=firstName;
-        this.LastNAme=LastNAme;
+        this.LastName=LastName;
         this.Address=Address;
         this.city=city;
         this.state=state;
@@ -18,5 +21,38 @@ public class AddressBook {
         this.phoneNumber=phoneNumber;
         this.email=email;
 
-    }}
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> contact= new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first name:");
+        String first_name= sc.nextLine();
+        contact.add(first_name);
+        System.out.println("Enter last name :");
+        String last_name=sc.nextLine();
+        contact.add(last_name);
+        System.out.println("Enter Address: ");
+        String address=sc.nextLine();
+        contact.add(address);
+        System.out.println("Enter city: ");
+        String city=sc.nextLine();
+        contact.add(city);
+        System.out.println("Enter state: ");
+        String state=sc.nextLine();
+        contact.add(state);
+        System.out.println("Enter zip: ");
+        String zip=sc.nextLine();
+        contact.add(zip);
+        System.out.println("Enter phone number: ");
+        String phone_number=sc.nextLine();
+        contact.add(phone_number);
+        System.out.println("Enter email: ");
+        String email=sc.nextLine();
+        contact.add(email);
+        for(int i=0; i< contact.size();i++){
+            System.out.println(contact.get(i));
+        }
+    }
+}
 
