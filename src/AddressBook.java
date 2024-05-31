0253d45e09efa3da12adfa4316080da8268412d3
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-05-31 15:56:00 +0530

    Delete contact file add

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,58 +1,40 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    String firstName;
-    String LastName;
-    String Address;
-    String city;
-    String state;
-    String zip;
-    String phoneNumber;
-    String email;
+    public static boolean exit = true;
 
-    public AddressBook(String firstName, String LastName, String Address, String city, String state, String zip, String phoneNumber, String email){
-        this.firstName=firstName;
-        this.LastName=LastName;
-        this.Address=Address;
-        this.city=city;
-        this.state=state;
-        this.zip=zip;
-        this.phoneNumber=phoneNumber;
-        this.email=email;
+    public static void options() {
+        System.out.println("(1) Create New Contact\n(2) Edit Contact\n(3) Delete Contact\n(4) Display\n(5) Exit");
+        Scanner sc = new Scanner(System.in);
+        Contact obj = new Contact();
+        int option = sc.nextInt();
 
+        switch (option) {
+            case 1:
+                obj.addContact();
+                break;
+            case 2:
+                obj.updateContact();
+                break;
+            case 3:
+                obj.deleteInfo();
+                break;
+            case 4:
+                System.out.println(obj.getInfo());
+                break;
+            case 5:
+                exit = false;
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
     }
 
     public static void main(String[] args) {
-        ArrayList<String> contact= new ArrayList<>();
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter first name:");
-        String first_name= sc.nextLine();
-        contact.add(first_name);
-        System.out.println("Enter last name :");
-        String last_name=sc.nextLine();
-        contact.add(last_name);
-        System.out.println("Enter Address: ");
-        String address=sc.nextLine();
-        contact.add(address);
-        System.out.println("Enter city: ");
-        String city=sc.nextLine();
-        contact.add(city);
-        System.out.println("Enter state: ");
-        String state=sc.nextLine();
-        contact.add(state);
-        System.out.println("Enter zip: ");
-        String zip=sc.nextLine();
-        contact.add(zip);
-        System.out.println("Enter phone number: ");
-        String phone_number=sc.nextLine();
-        contact.add(phone_number);
-        System.out.println("Enter email: ");
-        String email=sc.nextLine();
-        contact.add(email);
-        for(int i=0; i< contact.size();i++){
-            System.out.println(contact.get(i));
+        System.out.println("******Welcome to Address Book*******");
+        while (exit) {
+            options();
         }
     }
 }
-
